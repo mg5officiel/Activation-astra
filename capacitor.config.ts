fae693e7fb5907c46da0a78bb2c7f74b8c4c44a3
Massama GOITA <160361267+mg5officiel@users.noreply.github.com>
2026-09-22 20:10:21 +0000

    feat: rename mobile app to Astra Key

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.gestioncyber.keygen',
-  appName: 'Clés Cyber',
+  appName: 'Astra Key',
   webDir: 'dist',
 };
 
